b2f6279fd1350d6b3c38384de838d03bf3493381
Zver Industries <97319632+Borovnica32@users.noreply.github.com>
2026-05-12 14:37:33 +0200

    Update SenderProgram.cs

--- rabbitmq/SenderProgram.cs
+++ rabbitmq/SenderProgram.cs
@@ -1,20 +1,27 @@
-﻿// SENDER
+// SENDER
+
+// Manulal message sending in RabbitMQ
+//  1. Navigate to exchanges
+//  2. Select a exchange
+//  3. Provide routing key
+//  4. Provide message body (JSON)
 
 using RabbitMQ.Client;
 using System.Text;
 using System.Text.Json;
 using RabbitShared;
+using System;
 
 // Object responsible for creating connections to RabbitMQ
 ConnectionFactory factory = new();
 
 // Location where RabbitMQ is running
 //
-// amqp://  -> RabbitMQ protocol
-// guest    -> username
-// guest    -> password
-// localhost-> RabbitMQ server address
-// 5672     -> RabbitMQ default port
+// amqp://   -> RabbitMQ protocol
+// guest     -> username
+// guest     -> password
+// localhost -> RabbitMQ server address
+// 5672      -> RabbitMQ default port
 factory.Uri = new Uri("amqp://guest:guest@localhost:5672");
 
 // Human-readable connection name (visible in RabbitMQ dashboard/logs)
@@ -30,15 +37,15 @@ IChannel channel = await cnn.CreateChannelAsync();
 
 // Exchange receives messages from producers
 // and decides where they should go
-string exchangeName = "DemoExchange";
+string exchangeName = "MQExchange";
 
 // Routing key acts like an address/tag
 // Exchange uses it to decide which queue
 // should receive the message
-string routingKey = "demo-routing-key";
+string routingKey = "mq-routing-key";
 
 // Queue stores messages until consumers read them
-string queueName = "DemoQueue";
+string queueName = "DMQQueue";
 
 // Creates the exchange if it doesn't already exist
 //
@@ -49,9 +56,9 @@ await channel.ExchangeDeclareAsync(exchangeName, ExchangeType.Direct);
 // Creates queue if it doesn't already exist
 //
 // Parameters:
-//     durable     -> false = queue disappears after RabbitMQ restart
-//     exclusive   -> false = multiple consumers can access queue
-//     autoDelete  -> false = queue won't delete automatically
+//     durable    -> false = queue disappears after RabbitMQ restart
+//     exclusive  -> false = multiple consumers can access queue
+//     autoDelete -> false = queue won't delete automatically
 await channel.QueueDeclareAsync(queueName, false, false, false, null);
 
 // Connects:
@@ -77,11 +84,17 @@ for (int i = 0; i < 60; i++)
     MessageFormat data = new()
     {
         Id = i,
-        Message = $"Message #{i}"
+        Message = $"Message #{i}",
+        messageTimeStamp = $"Time of message: {DateTime.Now}",
+        userName = $"Sender name: {Environment.UserName}"
     };
 
-    // Convert object -> JSON
+    // Calculate message size
     string json = JsonSerializer.Serialize(data);
+    data.MessageSizeBytes = Encoding.UTF8.GetByteCount(json);
+
+    // Serialize final version
+    json = JsonSerializer.Serialize(data);
 
     Console.WriteLine($"Sending: {json}");
 
