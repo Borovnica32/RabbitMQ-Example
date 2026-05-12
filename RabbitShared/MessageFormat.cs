97a2a5851203b22b38c04c21559c4a1221ac8fb7
Zver Industries <97319632+Borovnica32@users.noreply.github.com>
2026-05-12 14:37:51 +0200

    Update MessageFormat.cs

--- RabbitShared/MessageFormat.cs
+++ RabbitShared/MessageFormat.cs
@@ -1,9 +1,15 @@
-﻿namespace RabbitShared
+namespace RabbitShared
 {
     public class MessageFormat
     {
         public int Id { get; set; }
 
         public required string Message { get; set; }
+
+        public required string messageTimeStamp { get; set; }
+
+        public required string userName { get; set; }
+            
+        public int MessageSizeBytes { get; set; }
     }
 }
